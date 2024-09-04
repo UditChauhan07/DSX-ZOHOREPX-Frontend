acdabeac3853f936bf2fd6bddb001bbca81095b0
Udit Chuahan <110827659+UditChauhan07@users.noreply.github.com>
2024-09-04 17:40:57 +0530

    Update config.js

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -1,4 +1,4 @@
-// const BASE_API_URL = "https://lms.designersx.com"; 
+const BASE_API_URL = "https://lms.designersx.com"; 
 
-const BASE_API_URL = "http://localhost:5000";
+//const BASE_API_URL = "http://localhost:5000";
 export default BASE_API_URL;
